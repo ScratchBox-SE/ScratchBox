4749e9ec062a9cd948b7fe7430bde0dd904b3479
Grady Link <github@grady.link>
2026-01-01 01:40:02 -0800

    fix(db): set original_id column in migration

--- server/database/migrations/0007_comments_editing.sql
+++ server/database/migrations/0007_comments_editing.sql
@@ -9,7 +9,7 @@ CREATE TABLE `__new_project_comments` (
 	FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
-INSERT INTO `__new_project_comments`("id", "project_id", "user", "body", "created_at") SELECT "id", "project_id", "user", "body", "created_at" FROM `project_comments`;--> statement-breakpoint
+INSERT INTO `__new_project_comments`("id", "original_id", "project_id", "user", "body", "created_at") SELECT "id", "id", "project_id", "user", "body", "created_at" FROM `project_comments`;--> statement-breakpoint
 DROP TABLE `project_comments`;--> statement-breakpoint
 ALTER TABLE `__new_project_comments` RENAME TO `project_comments`;--> statement-breakpoint
 PRAGMA foreign_keys=ON;
