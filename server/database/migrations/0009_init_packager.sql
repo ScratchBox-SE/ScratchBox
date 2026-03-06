88ea3e763cf644d2acf57a522098df66099f12be
Grady Link <github@grady.link>
2026-03-05 17:43:18 -0800

    chore: remove old migration sql

--- server/database/migrations/0009_init_packager.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `package_tasks` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`project_id` text NOT NULL,
-	`status` text DEFAULT 'pending' NOT NULL,
-	`output_path` text,
-	FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`) ON UPDATE no action ON DELETE no action
-);
