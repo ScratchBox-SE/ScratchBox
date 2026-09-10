94f1543c55f334725f126b3858c97cec10364b17
gradylink <github@grady.link>
2026-09-10 00:10:35 -0700

    fix: actually support logout

--- server/api/auth/logout.ts
+++ server/api/auth/logout.ts
@@ -1,7 +1,12 @@
 export default defineEventHandler(async (event) => {
   setCookie(event, "SB_TOKEN", "", {
     httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
     maxAge: 0,
+    domain: process.env.NODE_ENV === "production"
+      ? getRequestURL(event).hostname
+      : undefined,
   });
   sendRedirect(event, "/", 303);
 });
